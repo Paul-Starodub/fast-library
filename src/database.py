be035460e37e8dc7b2834be1822e0c697b1bf997
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-02-02 11:51:48 +0200

    add initial configuration and settings

---
+++ src/database.py
@@ -0,0 +1,7 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from src.config import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=settings.ECHO)
+
+# Prevent attribute expiration on commit to avoid async lazy-loads in response serialization.
+SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
