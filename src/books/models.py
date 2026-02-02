69f05784e1369471f3e16b68a378575c74011e08
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-02-02 12:10:17 +0200

    add book models

---
+++ src/books/models.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from src.models import Base
+
+
+class Genre(Base):
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    books: Mapped[list["Book"]] = relationship("Book", back_populates="genre", cascade="all, delete-orphan")
+
+    def __repr__(self) -> str:
+        return f"Genre(id={self.id}, name={self.name})"
+
+
+class Book(Base):
+    title: Mapped[str] = mapped_column(String(100), unique=True)
+    rating: Mapped[int] = mapped_column(default=0, ge=0)
+    date_published: Mapped[datetime]
+    image_file: Mapped[str | None] = mapped_column(String(200), nullable=True, default=None)
+    genre_id: Mapped[int] = mapped_column(ForeignKey("genre.id"), nullable=False)
+    genre: Mapped[Genre] = relationship("Genre", back_populates="books")
+
+    @property
+    def image_path(self) -> str:
+        if self.image_file:
+            return f"/media/book_pics/{self.image_file}"
+        return "/static/book_pics/default.jpg"
+
+    def __repr__(self) -> str:
+        return f"Book(id={self.id}, rating={self.rating}, date_published={self.date_published}"
--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
+app.mount("/media", StaticFiles(directory="media"), name="media")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
