45558da903f23e6aa7a566a2776fa48e48721d40
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-02-11 14:28:07 +0200

    add demo authentication logic

--- src/demo_auth/demo_jwt_auth.py
+++ src/demo_auth/demo_jwt_auth.py
@@ -1,114 +1,71 @@
-from jwt.exceptions import InvalidTokenError
 from fastapi import (
     APIRouter,
     Depends,
-    Form,
-    HTTPException,
-    status,
 )
 from fastapi.security import (
-    # HTTPBearer,
+    HTTPBearer,
     # HTTPAuthorizationCredentials,
-    OAuth2PasswordBearer,
 )
 from pydantic import BaseModel
 
-from src.auth import utils as auth_utils
+from src.demo_auth.helpers import (
+    create_access_token,
+    create_refresh_token,
+)
+from src.demo_auth.validation import (
+    get_current_token_payload,
+    get_current_auth_user_for_refresh,
+    get_current_active_auth_user,
+    validate_auth_user,
+    # REFRESH_TOKEN_TYPE,
+    # get_auth_user_from_token_of_type,
+    # UserGetterFromToken,
+)
 from src.authors.schemas import UserSchema
 
-# http_bearer = HTTPBearer()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/demo-auth/jwt/login/")
+http_bearer = HTTPBearer(auto_error=False)
 
 
 class TokenInfo(BaseModel):
     access_token: str
-    token_type: str
+    refresh_token: str | None = None
+    token_type: str = "Bearer"
 
 
-router = APIRouter(tags=["JWT"], prefix="/jwt")
-
-john = UserSchema(
-    username="john",
-    password=auth_utils.hash_password("qwerty"),
-    email="john@example.com",
+router = APIRouter(
+    prefix="/jwt",
+    tags=["JWT"],
+    dependencies=[Depends(http_bearer)],
 )
-sam = UserSchema(
-    username="sam",
-    password=auth_utils.hash_password("secret"),
-)
-
-users_db: dict[str, UserSchema] = {
-    john.username: john,
-    sam.username: sam,
-}
-
-
-def validate_auth_user(username: str = Form(), password: str = Form()):
-    unauthed_exc = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="invalid username or password",
-    )
-    if not (user := users_db.get(username)):
-        raise unauthed_exc
-
-    if not auth_utils.validate_password(password=password, hashed_password=user.password):
-        raise unauthed_exc
-
-    if not user.active:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="user inactive",
-        )
-
-    return user
-
-
-def get_current_token_payload(
-    # credentials: HTTPAuthorizationCredentials = Depends(http_bearer),
-    token: str = Depends(oauth2_scheme),
-) -> dict:
-    # token = credentials.credentials
-    try:
-        payload = auth_utils.decode_jwt(token=token)
-    except InvalidTokenError as e:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f"invalid token error: {e}",
-            # detail=f"invalid token error",
-        )
-    return payload
-
-
-def get_current_auth_user(
-    payload: dict = Depends(get_current_token_payload),
-) -> UserSchema:
-    username: str | None = payload.get("sub")
-    if user := users_db.get(username):
-        return user
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="token invalid (user not found)")
-
-
-def get_current_active_auth_user(
-    user: UserSchema = Depends(get_current_auth_user),
-):
-    if user.active:
-        return user
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="user inactive")
 
 
 @router.post("/login/", response_model=TokenInfo)
 def auth_user_issue_jwt(
     user: UserSchema = Depends(validate_auth_user),
 ):
-    jwt_payload = {
-        # subject
-        "sub": user.username,
-        "username": user.username,
-        "email": user.email,
-        # "logged_in_at"
-    }
-    token = auth_utils.encode_jwt(jwt_payload)
-    return TokenInfo(access_token=token, token_type="Bearer")
+    access_token = create_access_token(user)
+    refresh_token = create_refresh_token(user)
+    return TokenInfo(
+        access_token=access_token,
+        refresh_token=refresh_token,
+    )
+
+
+@router.post(
+    "/refresh/",
+    response_model=TokenInfo,
+    response_model_exclude_none=True,
+)
+def auth_refresh_jwt(
+    # todo: validate user is active!!
+    user: UserSchema = Depends(get_current_auth_user_for_refresh),
+    # user: UserSchema = Depends(get_auth_user_from_token_of_type(REFRESH_TOKEN_TYPE)),
+    # user: UserSchema = Depends(UserGetterFromToken(REFRESH_TOKEN_TYPE)),
+):
+    access_token = create_access_token(user)
+    return TokenInfo(
+        access_token=access_token,
+    )
 
 
 @router.get("/users/me/")
@@ -117,4 +74,8 @@ def auth_user_check_self_info(
     user: UserSchema = Depends(get_current_active_auth_user),
 ):
     iat = payload.get("iat")
-    return {"username": user.username, "email": user.email, "logged_in_at": iat}
+    return {
+        "username": user.username,
+        "email": user.email,
+        "logged_in_at": iat,
+    }
